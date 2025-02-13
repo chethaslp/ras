4b1403261ee274a912c59b2c064c8004967a0dae
chethas.lp <lchethas@gmail.com>
2025-02-13 09:50:38 +0530

    ADD: tinkercad

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
     <div className="flex items-center justify-center flex-col gap-4">
       <a target="_blank" href="https://docs.google.com/document/d/1gS_Ap9QM57To1uVzHiZ_-0OCbvwSfiq2-dBC9vYK9Fs/view" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Docs Link</a>
       <a target="_blank" href="https://wokwi.com/projects/421516276154954753" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Simulation Link</a>
-      <a target="_blank" href="https://www.tinkercad.com/things/f2RGBgmmBta-spectacular-kup/editel" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Tinkercad Link</a>
+      <a target="_blank" href="https://www.tinkercad.com/things/f2RGBgmmBta-buildabot-ieee-ras-ucek?sharecode=y_B9OBfzggxkAlaoNc_nP1pgGhBrZkfU-F_jDKiWa8Y" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Tinkercad Link</a>
+      
     </div>
   </div>
 }
